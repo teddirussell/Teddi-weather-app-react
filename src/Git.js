6b7bbc7230b27b50b0e7579442b901f6aebf8536
teddirussell <74998818+teddirussell@users.noreply.github.com>
2022-10-24 08:53:16 +1100

    Change React embed

--- src/Git.js
+++ src/Git.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 
 export default function Git() {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { StrictMode } from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
