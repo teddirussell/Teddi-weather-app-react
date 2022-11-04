2c2e3ee176423fb330352ccfc06b37c24a845bd0
teddirussell <74998818+teddirussell@users.noreply.github.com>
2022-11-04 14:02:34 +1100

    Current Weather Tweaks

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -7,29 +7,25 @@ export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
-      <ul>
-        <li>
-          <FormattedDate date={props.data.date} />
-        </li>
-        <li className="text-capitalize">{props.data.description}</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="d-flex">
+     <h2><FormattedDate date={props.data.date} /></h2>
+          
+        <h3 className="text-capitalize">{props.data.description}</h3>
+
+        <div className="row">
+  <div className="col-6">
+    <div className="d-flex current-weather">
             <div>
-              <WeatherIcon code={props.data.icon} size={52} />
+              <WeatherIcon code={props.data.icon} size={52} className="float-left" />
             </div>
 
-            <div>
-              <WeatherTemperature celsius={props.data.temperature} />
+            <div className="float-left">
+              <h2 className="weather"><WeatherTemperature celsius={props.data.temperature} /></h2>
             </div>
           </div>
         </div>
         <div className="col-6">
-          <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind} km/h</li>
-          </ul>
+        <p>Humidity: {props.data.humidity}%</p>
+        <p>Wind: {props.data.wind} km/h</p>
         </div>
       </div>
     </div>
