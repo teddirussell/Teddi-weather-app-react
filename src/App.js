209d55127a95a2825bc3f561647ef236ecf6fb9e
teddirussell <74998818+teddirussell@users.noreply.github.com>
2022-11-04 15:59:55 +1100

    Code Shuffle 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
-
 import Weather from "./Weather";
-import "./Weather.css";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
+
 
 export default function App() {
   return (
