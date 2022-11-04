488c92ef6192e9e91be863b6e5c0f180301872d7
teddirussell <74998818+teddirussell@users.noreply.github.com>
2022-11-04 15:58:40 +1100

    Code Shuffle

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-import App from "./App";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
