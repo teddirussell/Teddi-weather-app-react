9ca477fe0141a18077d806b2a9fe13c66848119a
teddirussell <74998818+teddirussell@users.noreply.github.com>
2022-11-04 15:13:32 +1100

    Structure Change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
+import React from "react";
+
 import Weather from "./Weather";
+import "./Weather.css";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div className="App">
+        <div className="container">
           <Weather defaultCity="New York" />
+
+          <footer>
+          This project was coded by Teddi Russell
+          and is{" "}
+          <a
+            href="https://github.com/teddirussell/teddi-weather-app-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://harmonious-brigadeiros-101803.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+        </div>
         </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Weather from "./Weather";
-import Git from "./Git";
 import App from "./App";
-
-import "bootstrap/dist/css/bootstrap.css";
-
-import "./Weather.css";
-import "./App.css";
+import "./index.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -17,7 +11,5 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
-      <Weather />
-      <Git />
   </StrictMode>
 );
